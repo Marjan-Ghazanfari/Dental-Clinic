8e8479659b7ee86f08015beb9b048dbf43cf6c25
Marjan Ghazanfari <ghazanfari.marjan@gmail.com>
2021-07-12 15:39:06 +0200

    Marjan commit-About Us

--- dental-clinic/src/components/navigation/TopMenu.js
+++ dental-clinic/src/components/navigation/TopMenu.js
@@ -5,17 +5,17 @@ const TopMenu = () => {
     return (
         <div>
             <div className="top-wrapper">
-            <div className="topbar-openhours">
-                <i className="far fa-clock"></i>
-                <p>Working hours: Monday – Friday: 9:00 – 18:00</p>
+                <div className="topbar-openhours">
+                    <i className="far fa-clock"></i>
+                    <p>Working hours: Monday – Friday: 9:00 – 18:00</p>
+                </div>
+                <div className="topbar-socialmedia">
+                    <i className="fab fa-facebook-f"></i>
+                    <i className="fab fa-twitter"></i>
+                    <i className="fab fa-google-plus-g"></i>
+                    <i className="fab fa-instagram"></i>           
+                </div>
             </div>
-            <div className="topbar-socialmedia">
-                 <i className="fab fa-facebook-f"></i>
-                 <i className="fab fa-twitter"></i>
-                 <i className="fab fa-google-plus-g"></i>
-                 <i className="fab fa-instagram"></i>           
-            </div>
-        </div>
         </div>
     )
 }
