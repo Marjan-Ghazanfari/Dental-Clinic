d119afad6f4fe5febba857b347ebaf7279af1483
Marjan Ghazanfari <ghazanfari.marjan@gmail.com>
2021-07-12 08:58:37 +0200

    first commit

---
+++ dental-clinic/src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import TopMenu from './components/navigation/TopMenu';
+import MainMenu from './components/navigation/MainMenu';
+import ShowCase from './components/ShowCase';
+import AboutUs from './components/AboutUs';
+import Services from './components/Services';
+import Appointment from './components/Appointment';
+import Footer from './components/navigation/Footer';
+
+
+function App() {
+  return (
+    <div className="container">
+      <TopMenu />
+      <MainMenu />
+      <ShowCase />
+      <AboutUs />
+      <Services />
+      <Appointment />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ dental-clinic/src/components/ShowCase.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowCase = () => {
+    return (
+        <div>
+            Showcase
+        </div>
+    )
+}
+
+export default ShowCase
